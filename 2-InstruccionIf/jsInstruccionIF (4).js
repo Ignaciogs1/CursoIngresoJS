9033db07679bb1684ca627fc26e6f3909fc12454
Ignaciogs1 <ignaciogms@gmail.com>
2019-02-12 11:41:48 -0300

    actualización

--- 2-InstruccionIf/jsInstruccionIF (4).js
+++ 2-InstruccionIf/jsInstruccionIF (4).js
@@ -2,6 +2,14 @@ function mostrar()
 {
 //tomo la edad  
 
+	var laVariable;
 
+	laVariable=edad.value;
+	laVariable=parseInt(laVariable);
+
+	if(laVariable<18 && laVariable>12)
+	{
+		alert("eres adolescente");
+	}
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (5).js
+++ 2-InstruccionIf/jsInstruccionIF (5).js
@@ -1,6 +1,14 @@
 function mostrar()
 {
 //tomo la edad  
+	var laVariable;
 
+	laVariable=edad.value;
+	laVariable=parseInt(laVariable);
+
+	if(laVariable<13 || laVariable>17)
+	{
+		alert("no eres adolescente");
+	}
 
 }//FIN DE LA FUNCIÓN
