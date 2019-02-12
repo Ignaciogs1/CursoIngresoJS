8af5e87a2fb9b475576d5cf446122f2f4d90a784
Ignaciogs1 <ignaciogms@gmail.com>
2019-02-12 11:59:59 -0300

    actualización

--- 2-InstruccionIf/jsInstruccionIF (7).js
+++ 2-InstruccionIf/jsInstruccionIF (7).js
@@ -2,7 +2,15 @@ function mostrar()
 {
 //tomo la edad  
 
-	
+	var laVariable;
+
+	laVariable=edad.value;
+	laVariable=parseInt(laVariable);
+
+	if(laVariable<18)
+	{
+		alert("Es muy pequeño para NO ser soltero");
+	}
 
 
 }//FIN DE LA FUNCIÓN
