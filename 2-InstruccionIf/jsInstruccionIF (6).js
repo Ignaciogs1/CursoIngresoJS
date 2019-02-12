df5f7bdb19fc66bde196fee52f452f1f2e5a4d7c
Ignaciogs1 <ignaciogms@gmail.com>
2019-02-12 11:54:33 -0300

    actualización

--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -2,7 +2,23 @@ function mostrar()
 {
 //tomo la edad  
 
+	var laVariable;
 
+	laVariable=edad.value;
+	laVariable=parseInt(laVariable);
 
+	if(laVariable<13)
+	{
+		alert("Eres un/a niño/a");
+	}
 
+	if(laVariable>13 && laVariable<17)
+	{
+		alert("Eres un/a adolescente")
+	}
+
+	if(laVariable>18)
+	{
+		alert("Eres mayor de edad");
+	}
 }//FIN DE LA FUNCIÓN
